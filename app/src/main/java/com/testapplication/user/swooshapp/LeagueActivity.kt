22bca55035e81b4ae640ddfdc91b2231dfd6ad8c
user <Hami.cool15@gmail.com>
2018-09-23 20:32:49 +0500

    3rd Activity + Use of Intent

--- app/src/main/java/com/testapplication/user/swooshapp/LeagueActivity.kt
+++ app/src/main/java/com/testapplication/user/swooshapp/LeagueActivity.kt
@@ -1,12 +1,21 @@
 package com.testapplication.user.swooshapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        nextBtn.setOnClickListener {
+
+            val intent = Intent(this,SkillActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
--- app/src/main/java/com/testapplication/user/swooshapp/WelcomeActivity.kt
+++ app/src/main/java/com/testapplication/user/swooshapp/WelcomeActivity.kt
@@ -1,12 +1,22 @@
 package com.testapplication.user.swooshapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        getStartedbtn.setOnClickListener {
+
+            val intent = Intent(this,LeagueActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
